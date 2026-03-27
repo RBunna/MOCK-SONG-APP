4f930c01f5d92fd85a42fc1dab3ff4fb18480e48
RBunna <bunnasovethroath@gmail.com>
2026-03-27 08:40:27 +0700

    W9-02 – Fetch and display artists in the Artist screen

---
+++ lib/ui/screens/artist/view_model/artist_view_model.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import '../../../../data/repositories/artists/artist_repository.dart';
+import '../../../../model/artists/artist.dart';
+import '../../../utils/async_value.dart';
+
+class ArtistViewModel extends ChangeNotifier {
+  final ArtistRepository artistRepository;
+
+  AsyncValue<List<Artist>> artistsValue = AsyncValue.loading();
+
+  ArtistViewModel({required this.artistRepository}) {
+    _init();
+  }
+
+  void _init() async {
+    fetchArtist();
+  }
+
+  void fetchArtist() async {
+    // 1- Loading state
+    artistsValue = AsyncValue.loading();
+    notifyListeners();
+
+    try {
+      // 2- Fetch is successfull
+      List<Artist> artists = await artistRepository.fetchArtists();
+      artistsValue = AsyncValue<List<Artist>>.success(artists);
+    } catch (e) {
+      // 3- Fetch is unsucessfull
+      artistsValue = AsyncValue.error(e);
+    }
+    notifyListeners();
+  }
+}
